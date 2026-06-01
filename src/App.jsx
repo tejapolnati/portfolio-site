68b0402c481730aec1581a05fc7bac2482fa65a9
teja polnati <tejapolnati5769@gmail.com>
2026-06-01 13:03:47 +0530

    Update portfolio design

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+import "./App.css";
 function App() {
   return (
     <>
